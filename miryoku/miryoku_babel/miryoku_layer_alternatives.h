1079c6971e11d92ac5d852e9a21cad5c4b0695f4
fouf96 <rafthun@gmail.com>
2024-06-02 16:56:48 +0200

    switch QWERTY keymap, homerow mod, right side to R-SHFT/CTRL/ALT/GUI

--- miryoku/miryoku_babel/miryoku_layer_alternatives.h
+++ miryoku/miryoku_babel/miryoku_layer_alternatives.h
@@ -116,7 +116,7 @@ U_NP,              U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_NAV, SPACE),U_LT
 
 #define MIRYOKU_ALTERNATIVES_BASE_QWERTY \
 &kp Q,             &kp W,             &kp E,             &kp R,             &kp T,             &kp Y,             &kp U,             &kp I,             &kp O,             &kp P,             \
-U_MT(LCTRL, A),     U_MT(LALT, S),     U_MT(LSHIFT, D),    U_MT(LGUI, F),    &kp G,             &kp H,             U_MT(LGUI, J),    U_MT(LSHIFT, K),    U_MT(LALT, L),     U_MT(LCTRL, SQT),   \
+U_MT(LCTRL, A),     U_MT(LALT, S),     U_MT(LSHIFT, D),    U_MT(LGUI, F),    &kp G,             &kp H,             U_MT(RGUI, J),    U_MT(RSHIFT, K),    U_MT(RALT, L),     U_MT(RCTRL, SQT),   \
 U_LT(U_BUTTON, Z), U_MT(RALT, X),     &kp C,             &kp V,             &kp B,             &kp N,             &kp M,             &kp COMMA,         U_MT(RALT, DOT),   U_LT(U_BUTTON, SLASH),\
 U_NP,              U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_NAV, SPACE),U_LT(U_MOUSE, TAB),U_LT(U_SYM, RET),  U_LT(U_NUM, BSPC), U_LT(U_FUN, DEL),  U_NP,              U_NP
 
